18a83a6c00d9634fe42cedf04b3f77d5aa71dd64
Jordan Wilkerson <jordangwilkerson@gmail.com>
2021-02-26 16:30:34 -0700

    where are you

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -3,15 +3,24 @@
 export default class Weather{
     constructor(data){
         this.temp = data.main.temp,
-        this.weather = data.weather.main
+        this.weather = data.weather
     }
-
+    
+    getTypes(){
+        let types = " "
+        for (let i = 0; i< this.weather.length; i++){
+        
+        types = types+(this.weather[i].main)+" "
+        }
+        return types
+    }
 
     get Template() {
         return /*html*/ `
        
         <div class="card-body">
-            <p class ="d-inline">${this.weather}++${this.temp}</p>
+            <p class ="d-inline">${this.getTypes()}</p><br>
+            <p class ="d-inline">${this.temp}°K</p>
         </div>
     </div>`
       }
